abbe0a89cd3c25ce01626c7a658db208c1032310
Mohammad Ridwan <80090718+akang-one@users.noreply.github.com>
2022-04-26 04:43:44 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://akang-one.github.io/';
+$config['base_url'] = 'https://akang-one.github.io/cat-shop-069/';
 
 /*
 |--------------------------------------------------------------------------
